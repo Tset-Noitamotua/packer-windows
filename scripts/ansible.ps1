51c73ed17cef46d3bc6b56e2b6040a57c96e3e02
Alexander Komlev <aleksandr.komlev@gmail.com>
2016-08-09 18:03:24 +0300

    Fixed errors in ansible preconfiguration

--- scripts/ansible.ps1
+++ scripts/ansible.ps1
@@ -18,69 +18,68 @@
 # 6.3 is 2012 R2
 
 $powershellpath = "C:\powershell"
-
-if ($PSVersionTable.psversion.Major -ge 3) {
-    Write-Host "Powershell 3 Installed already; You don't need this"
-    Exit
+if (!(Test-Path $powershellpath)) {
+    New-Item -ItemType directory -Path $powershellpath
 }
 
-function download-file {
+function Download-File {
     param ([string]$path, [string]$local)
-    $client = new-object system.net.WebClient
+    $client = New-Object system.net.WebClient
     $client.Headers.Add("user-agent", "PowerShell")
     $client.downloadfile($path, $local)
 }
 
-if (!(test-path $powershellpath)) {
-    New-Item -ItemType directory -Path $powershellpath
-}
+if (!($PSVersionTable.psversion.Major -ge 3)) {
+    # .NET Framework 4.0 is necessary.
+    if (($PSVersionTable.CLRVersion.Major) -le 2) {
+        $DownloadUrl = "http://download.microsoft.com/download/B/A/4/BA4A7E71-2906-4B2D-A0E1-80CF16844F5F/dotNetFx45_Full_x86_x64.exe"
+        $FileName = $DownLoadUrl.Split('/')[-1]
+        Write-Host "Downloading .NET 4.5 -> $downloadurl"
+        Download-File $downloadurl "$powershellpath\$filename"
+        Write-Host "Installing .NET 4.5 ..."
+        Start-Process -Wait -FilePath "$powershellpath\$filename" -ArgumentList "/quiet", "/norestart"
+    }
 
-# .NET Framework 4.0 is necessary.
-if (($PSVersionTable.CLRVersion.Major) -le 2) {
-    $DownloadUrl = "http://download.microsoft.com/download/B/A/4/BA4A7E71-2906-4B2D-A0E1-80CF16844F5F/dotNetFx45_Full_x86_x64.exe"
+    # You may need to reboot after the .NET install if so just run the script again.
+    # If the Operating System is above 6.2, then you already have PowerShell Version > 3
+    if ([Environment]::OSVersion.Version.Major -gt 6) {
+        Write-Host "OS is new; upgrade not needed."
+        Exit
+    }
+
+    $DownloadUrl="https://download.microsoft.com/download/3/D/6/3D61D262-8549-4769-A660-230B67E15B25/Windows6.1-KB2819745-x64-MultiPkg.msu"
     $FileName = $DownLoadUrl.Split('/')[-1]
-    Write-Host "Downloading .NET 4.5 -> $downloadurl"
-    download-file $downloadurl "$powershellpath\$filename"
-    Write-Host "Installing .NET 4.5 ..."
-    Start-Process -Wait -FilePath "$powershellpath\$filename" -ArgumentList "/quiet", "/norestart"
+    Write-Host "Downloading WMF 4.0 update -> $downloadurl ..."
+    Download-File $downloadurl "$powershellpath\$filename"
+    Write-Host "Installing WMF 4.0 update ..."
+    Start-Process -Wait -FilePath "$powershellpath\$filename" -ArgumentList "/quiet"
+    Write-Host "WMF 4.0 update installed"
+} else {
+    Write-Host "Powershell 3 Installed already; You don't need this"
 }
-
-# You may need to reboot after the .NET install if so just run the script again.
-# If the Operating System is above 6.2, then you already have PowerShell Version > 3
-if ([Environment]::OSVersion.Version.Major -gt 6) {
-    Write-Host "OS is new; upgrade not needed."
-    Exit
-}
-
-$DownloadUrl="https://download.microsoft.com/download/3/D/6/3D61D262-8549-4769-A660-230B67E15B25/Windows6.1-KB2819745-x64-MultiPkg.msu"
-$FileName = $DownLoadUrl.Split('/')[-1]
-Write-Host "Downloading WMF 4.0 update -> $downloadurl ..."
-download-file $downloadurl "$powershellpath\$filename"
-Write-Host "Installing WMF 4.0 update ..."
-Start-Process -Wait -FilePath "$powershellpath\$filename" -ArgumentList "/quiet"
-Write-Host "WMF 4.0 update installed"
-
 
 # Configure for Ansible
 Write-Host "Configuring Ansible for Windows ..."
 $DownloadUrl="https://raw.githubusercontent.com/cchurch/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
 $FileName = $DownLoadUrl.Split('/')[-1]
-download-file $downloadurl "$powershellpath\$filename"
-Start-Process powershell.exe -ArgumentList "-Wait", "-FilePath $powershellpath\$filename"
+Download-File $downloadurl "$powershellpath\$filename"
+Write-Host "$powershellpath\$filename"
+Start-Process powershell.exe -Wait -ArgumentList "$powershellpath\$filename"
 Write-Host "Ansible for Windows configured"
 
 
 # Install Chocolatey
 $chocoExePath = 'C:\ProgramData\Chocolatey\bin'
-if ($($env:Path).ToLower().Contains($($chocoExePath).ToLower())) {
-  echo "Chocolatey found in PATH, skipping install..."
-  Exit
+if (!$($env:Path).ToLower().Contains($($chocoExePath).ToLower())) {
+    # Add to system PATH
+    $systemPath = [Environment]::GetEnvironmentVariable('Path',[System.EnvironmentVariableTarget]::Machine)
+    $systemPath += ';' + $chocoExePath
+    [Environment]::SetEnvironmentVariable("PATH", $systemPath, [System.EnvironmentVariableTarget]::Machine)
+    iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+} else {
+    echo "Chocolatey found in PATH, skipping install..."
 }
-# Add to system PATH
-$systemPath = [Environment]::GetEnvironmentVariable('Path',[System.EnvironmentVariableTarget]::Machine)
-$systemPath += ';' + $chocoExePath
-[Environment]::SetEnvironmentVariable("PATH", $systemPath, [System.EnvironmentVariableTarget]::Machine)
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
 
 # Install OpenSSH and start it
-Start-Process powershell.exe -ArgumentList "-Wait", "-FilePath", "a:\openssh.ps1", "-AutoStart"
+Start-Process powershell.exe -Wait -ArgumentList "-NoExit", "a:\openssh.ps1", "-AutoStart"
